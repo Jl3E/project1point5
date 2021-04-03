e6f8ecf9010990b8a47765c13c2531bb285c88fc
Joshua <swansonj@mail.usf.edu>
2021-04-02 20:56:08 -0400

    finished delete servlet for reimbursements

--- 0.0.1/src/main/java/com/project1point5/servlet/reimbursementServlet/ReimbursementDeleteServlet.java
+++ 0.0.1/src/main/java/com/project1point5/servlet/reimbursementServlet/ReimbursementDeleteServlet.java
@@ -1,4 +1,116 @@
 package com.project1point5.servlet.reimbursementServlet;
 
-public class ReimbursementDeleteServlet {
+import com.project1point5.dao.ReimbursementDao;
+import com.project1point5.model.Reimbursement;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+
+/**
+ * Deletes reimbursement from DB using HTTP Delete
+ *      Reads request body. Json must be formatted like:
+ *
+ * {
+ * "reimbursement": {
+ * "id": 1,
+ * "amount": 50,
+ * "submitted": "2021-04-02 17:56:11.739",
+ * "resolved": "2021-04-02 17:56:11.739",
+ * "description": "something",
+ * "author": 1,
+ * "resolver": 1,
+ * "status_id": 1,
+ * "type_id": 1
+ * }
+ * }
+ */
+
+@WebServlet("/reimbursement/delete")
+public class ReimbursementDeleteServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("No use for doGet()");
+        out.flush();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("No use for doPut()");
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("No use for doPost()");
+        out.flush();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        delete(req, resp);
+    }
+
+    /**
+     * Handles doDelete()
+     */
+    public void delete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Reimbursement reimbursement;
+        PrintWriter out = resp.getWriter();
+        StringBuilder jsonBuffer = new StringBuilder();
+        String line;
+
+        //Try reading body
+        try{
+            BufferedReader reader = req.getReader();
+            while((line = reader.readLine()) != null){
+                jsonBuffer.append(line);
+            }
+        }catch (Exception e){
+            out = resp.getWriter();
+            out.write("Couldn't read body");
+        }
+
+        //Try to create reimbursement with json body contents
+        try{
+            reimbursement = getReimbursementFromJsonString(jsonBuffer);
+            ReimbursementDao reimbursementDao = new ReimbursementDao();
+            reimbursementDao.delete(reimbursement);
+            out.print("Success");
+        }catch(Exception e){
+            out.print("Failure");
+        }
+    }
+
+    /**
+     * Uses a json string to build a reimbursement object
+     */
+    public Reimbursement getReimbursementFromJsonString(StringBuilder jsonBuffer){
+        JSONObject jsonObject = new JSONObject(jsonBuffer.toString());
+        int id = jsonObject.getJSONObject("reimbursement").getInt("id");
+        float amount = jsonObject.getJSONObject("reimbursement").getFloat("amount");
+        String s = jsonObject.getJSONObject("reimbursement").getString("submitted");
+        String r = jsonObject.getJSONObject("reimbursement").getString("resolved");
+        String description = jsonObject.getJSONObject("reimbursement").getString("description");
+        int author = jsonObject.getJSONObject("reimbursement").getInt("author");
+        int resolver = jsonObject.getJSONObject("reimbursement").getInt("resolver");
+        int status_id = jsonObject.getJSONObject("reimbursement").getInt("status_id");
+        int type_id = jsonObject.getJSONObject("reimbursement").getInt("type_id");
+        Timestamp submitted = Timestamp.valueOf(s);
+        Timestamp resolved = Timestamp.valueOf(r);
+
+        return new Reimbursement(id, amount, submitted, resolved, description, author, resolver, status_id, type_id);
+    }
+
 }
