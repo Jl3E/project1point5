57e8454f257e0106def97983ff6d8002ab002788
Joshua <swansonj@mail.usf.edu>
2021-04-05 10:04:33 -0400

    Reimbursement tests

--- 0.0.1/src/test/java/com/project1point5/servlet/reimbursementServlet/ReimbursementInsertServletTest.java
+++ 0.0.1/src/test/java/com/project1point5/servlet/reimbursementServlet/ReimbursementInsertServletTest.java
@@ -4,54 +4,50 @@ import com.project1point5.service.ReimbursementService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-public class ReimbursementDeleteServletTest extends Mockito {
+class ReimbursementInsertServletTest extends Mockito {
 
     HttpServletRequest request = mock(HttpServletRequest.class);
     HttpServletResponse response = mock(HttpServletResponse.class);
     ReimbursementService reimbursementService = mock(ReimbursementService.class);
 
     @Test
-    public void testDoGet() throws Exception{
+    void doGet() throws IOException, ServletException {
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(writer);
 
-        new ReimbursementGetListServlet().doGet(request, response);
+        new ReimbursementGetByIdServlet().doGet(request, response);
         assertFalse(stringWriter.toString().isEmpty());
     }
 
     @Test
-    public void testDoPost() throws Exception{
+    void doPut() throws IOException, ServletException {
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(writer);
 
-        new ReimbursementGetListServlet().doPost(request, response);
+        new ReimbursementGetByIdServlet().doPut(request, response);
         assertFalse(stringWriter.toString().isEmpty());
     }
 
     @Test
-    public void testDoPut() throws Exception{
+    void doDelete() throws IOException, ServletException {
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(writer);
 
-        new ReimbursementGetListServlet().doPut(request, response);
+        new ReimbursementGetByIdServlet().doDelete(request, response);
         assertFalse(stringWriter.toString().isEmpty());
     }
 
-    @Test
-    public void testJsonBuilder() throws Exception{
-        StringBuilder jsonBulder = new StringBuilder();
-        jsonBulder.append( "{\"reimbursement\": {\"id\": 200}}");
-        assertEquals(new ReimbursementDeleteServlet().getReimbursementFromJsonString(jsonBulder), null);
-    }
 }
